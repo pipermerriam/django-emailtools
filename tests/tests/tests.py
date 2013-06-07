f1f6f03b9c399ee438d685c8f706075835ca4bdf
Aaron Merriam <amerriam@fusionbox.com>
2013-06-06 21:34:15 -0600

    update tests to cover improper configuration

--- tests/tests/tests.py
+++ tests/tests/tests.py
@@ -1,6 +1,7 @@
 from django.core import mail
 from django.test import TestCase
 from django.test.utils import override_settings
+from django.core.exceptions import ImproperlyConfigured
 
 from emailtools import BasicEmail, HTMLEmail, MarkdownEmail
 
@@ -56,13 +57,34 @@ class TestBasicCBE(TestCase):
         self.assertEqual(message.bcc, ['overridden_bcc@example.com'])
         self.assertEqual(message.subject, 'overridden_subject')
 
+    def test_improper_settings_override(self):
+        with self.assertRaises(TypeError):
+            self.create_and_send_a_message(not_a_class_property=True)
+
     def test_to_address_as_string(self):
         self.create_and_send_a_message(to='string@example.com')
         message = mail.outbox[0]
         self.assertEqual(message.to, ['string@example.com'])
 
+    def test_missing_message_class(self):
+        with self.assertRaises(ImproperlyConfigured):
+            self.TestEmail.as_callable(email_message_class=None)()
+
     def test_missing_to(self):
-        self
+        with self.assertRaises(ImproperlyConfigured):
+            self.create_and_send_a_message(to=None)()
+
+    def test_missing_from(self):
+        with self.assertRaises(ImproperlyConfigured):
+            self.create_and_send_a_message(from_email=None)()
+
+    def test_missing_subject(self):
+        with self.assertRaises(ImproperlyConfigured):
+            self.create_and_send_a_message(subject=None)()
+
+    def test_missing_body(self):
+        with self.assertRaises(ImproperlyConfigured):
+            self.create_and_send_a_message(body=None)()
 
 
 class TestHTMLEmail(TestCase):
@@ -158,3 +180,9 @@ class TestMarkdownEmail(TestHTMLEmail):
     def create_and_send_a_message(self, **kwargs):
         email_callable = self.TestMarkdownEmail.as_callable(**kwargs)
         email_callable()
+
+    @override_settings(EMAIL_LAYOUT=None)
+    def test_missing_base_layout(self):
+        self.create_and_send_a_message()
+        with self.assertRaises(ImproperlyConfigured):
+            self.create_and_send_a_message(layout_template=None)
